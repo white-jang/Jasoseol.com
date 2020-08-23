eb8487481173ad448c92484f3327b44c646e05ed
white-jang <galaxy__12@naver.com>
2020-08-23 15:25:12 +0900

    heroku 배포 준비

--- JssProject/settings.py
+++ JssProject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y3j9j9s3oq($fu0-da!h7+qzx7ev@_6w3smu0h%_%_fs8evv%u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
